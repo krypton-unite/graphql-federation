d5a32f33d31f0a5c3e0408c393678d52ad49b6d7
Daniel Kaminski de Souza <daniel@kryptonunite.com>
2021-01-24 09:19:32 +0000

    :construction: Development path.

--- scripts/Push-Schema.ps1
+++ scripts/Push-Schema.ps1
@@ -1,8 +1,20 @@
 $location = Get-Location
 # Set child env
 Set-PsEnv
+$HashArguments = @{
+  "schema" = "$location/$env:LOCAL_SCHEMA_FILE"
+  "key" = $env:KEY
+  "graph" = $env:GRAPH
+  "variant" = $env:VARIANT
+  "service_name" = $env:SERVICE_NAME
+  "service_url" = $env:SERVICE_URL
+}
+# Write-Host $HashArguments["schema"]
+# Write-Host $HashArguments["service_name"]
+# Write-Host $HashArguments["service_url"]
 Set-Location -Path $PSScriptRoot/..
 # Set parent env
 Set-PsEnv
 . $PSScriptRoot/Apollo-Service-Push.ps1
-Apollo-Service-Push $location/$env:LOCAL_SCHEMA_FILE $env:KEY $env:GRAPH $env:VARIANT $env:SERVICE_NAME $env:SERVICE_URL
+# Write-Host $HashArguments["schema"] $env:KEY $env:GRAPH $env:VARIANT $HashArguments["service_name"] $HashArguments["service_url"]
+Apollo-Service-Push $HashArguments["schema"] $env:KEY $env:GRAPH $env:VARIANT $HashArguments["service_name"] $HashArguments["service_url"]
